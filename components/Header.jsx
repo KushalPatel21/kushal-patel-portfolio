892c3c47b602dca56eb9fa833065a10d09c8a060
Kushal Patel <kushal.rajani7@gmail.com>
2024-07-16 19:33:48 +1200

    navigation bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,29 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+//components
+import Nav from "./Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Kushal<span className="text-accent">.</span>
           </h1>
         </Link>
-        {/* desktop nav */}
-        <Nav />
+        {/* desktop nav  & hire me button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav*/}
+        <div className="xl:hidden">mobile</div>
       </div>
     </header>
   );
