c8388a213d82aec16a8058f007e5f1a2014d194d
Kushal Patel <kushal.rajani7@gmail.com>
2024-07-16 19:13:28 +1200

    header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,13 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const jetbrainsMono = JetBrains_Mono({ 
-  subsets: ["latin"], 
-  weight:["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono'
+//components
+import Header from "@/components/Header";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -15,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
